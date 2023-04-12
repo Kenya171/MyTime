2ccf93c8c24d1f2fc743d5cb340d0b3263cdf432
Kenya171 <fujikawa.k569@gmail.com>
2023-04-12 17:59:15 +0900

    タイマーアプリ作成

--- My Time/ContentView.swift
+++ My Time/ContentView.swift
@@ -9,15 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            NavigationStack{
+                VStack {
+                Text("タイマー画面")
+            }
+            //ナビゲーションにボタンを配置
+                .toolbar{
+                //ナビゲーションバーの右にボタンを追加
+                ToolbarItem(placement: .navigationBarTrailing){
+                    //ナビゲーション遷移
+                    NavigationLink{
+                        SettingView()
+                    } label: {
+                        //テキストを表示
+                        Text("秒数設定")
+                    }// NavigationLink ここまで
+                }// ToolbarItem ここまで
+            }// .toolbar　ここまで
+        }// NavigationStack ここまで
+    }//body　ここまで
+}// NavigationStack　ここまで
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ My Time/SettingView.swift
@@ -0,0 +1,20 @@
+//
+//  Setting View.swift
+//  My Time
+//
+//  Created by 藤川賢也 on 2023/04/12.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
