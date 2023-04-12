98d85152f23894bed88c98e7df3e499148942805
Kenya171 <fujikawa.k569@gmail.com>
2023-04-12 11:46:00 +0900

    Initial Commit

---
+++ My Time/My_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_TimeApp.swift
+//  My Time
+//
+//  Created by 藤川賢也 on 2023/04/12.
+//
+
+import SwiftUI
+
+@main
+struct My_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
