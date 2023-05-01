eaa01102fba8f0ed89dfb84c2d8b2d17e3a05dbe
Kenya171 <fujikawa.k569@gmail.com>
2023-05-01 13:40:21 +0900

    タイマーアプリ形だけ完成

--- My Time/ContentView.swift
+++ My Time/ContentView.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    //タイマーの変数を作成
+    @State var timerHandler : Timer?
+    //カウント（経過時間）の変数を作成
+    @State var count = 0
+    //永続化する秒数設定（初期値は10)
+    @AppStorage("timer_value") var timerValue = 10
+    
     var body: some View {
-            NavigationStack{
-        //　奥から手前方向にレイアウト
+        NavigationStack{
+            //　奥から手前方向にレイアウト
             ZStack{
                 //背景画像
                 Image("backgroundTimer")
@@ -19,27 +26,116 @@ struct ContentView: View {
                 //セーフエリアを超えて画面全体に配置します
                     .ignoresSafeArea()
                 //アスペクト比(縦横比)を維持して短辺基準に収まるようにする
-                    .scaledToFit()
+                    .scaledToFill()
+                //垂直にレイアウト（縦方向にレイアウト）
+                //View（部品）間の間隔を30にする
+                VStack(spacing: 30.0){
+                    //テキストを表示する
+                    Text("残り\(timerValue - count)秒")
+                    //文字のサイズを指定
+                        .font(.largeTitle)
+                    //水平にレイアウト（横方向にレイアウト）
+                    HStack{
+                        //スタートボタン
+                        Button {
+                            //ボタンをタップした時のアクション
+                            //タイマーをカウントダウン開始する関数を呼び出す
+                            startTimer()
+                        } label: {
+                            Text("スタート")
+                            //文字サイズを指定
+                                .font(.title)
+                            //文字色を白に指定
+                                .foregroundColor(Color.white)
+                            //縦高さを140に指定
+                                .frame(width: 140,height: 140)
+                            //背景を設定
+                                .background(Color("startColor"))
+                            //円形に切り抜く
+                                .clipShape(Circle())
+                            
+                        }//スタートボタンはここまで
+                        //ストップボタン
+                        Button{
+                            //ボタンをタップした時のアクション
+                            //taimerHandlerをアンラップしてunwrapedTimerHandlerに代入
+                            if let unwrapedTimerHandler = timerHandler{
+                                //もしタイマーが実行中だったら停止
+                                if unwrapedTimerHandler.isValid == true{
+                                    //タイマー停止
+                                    unwrapedTimerHandler.invalidate()
+                                }
+                            }
+                        }label:{
+                            Text("ストップ")
+                           
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(width: 140,height: 140)
+                                .background(Color("stopColor"))
+                                .clipShape(Circle())
+                        }
+                    }//HStackここまで
+                    }//VStackここまで
                 }//ZStackここまで
-            //ナビゲーションにボタンを配置
+                //画面が表示されるときに実行される
+            .onAppear{
+                //カウント（経過時間）の変数を初期化
+                count = 0
+            }
+                //ナビゲーションにボタンを配置
                 .toolbar{
-                //ナビゲーションバーの右にボタンを追加
-                ToolbarItem(placement: .navigationBarTrailing){
-                    //ナビゲーション遷移
-                    NavigationLink{
-                        SettingView()
-                    } label: {
-                        //テキストを表示
-                        Text("秒数設定")
-                    }// NavigationLink ここまで
-                }// ToolbarItem ここまで
-            }// .toolbar　ここまで
-        }// NavigationStack ここまで
-    }//body　ここまで
-}// NavigationStack　ここまで
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+                    //ナビゲーションバーの右にボタンを追加
+                    ToolbarItem(placement: .navigationBarTrailing){
+                        //ナビゲーション遷移
+                        NavigationLink{
+                            SettingView()
+                        } label: {
+                            //テキストを表示
+                            Text("秒数設定")
+                        }// NavigationLink ここまで
+                    }// ToolbarItem ここまで
+                }// .toolbar　ここまで
+            }// NavigationStack ここまで
+        }//body　ここまで
+    //1秒毎に実行されてカウントダウンする
+    func countDownTimer(){
+        //count(経過時間)に＋1していく
+        count += 1
+        //残り時間が0以下の時、タイマーを止める
+        if timerValue - count <= 0 {
+            //タイマー停止
+            timerHandler?.invalidate()
+        }
+    }//countDownTimerここまで
+    //タイマーをカウントダウン開始する関数
+    func startTimer(){
+        //timerHandlerをアンラップしてunwrapedTimerHandlerに代入
+        if let unwrapedTimerHandler = timerHandler{
+            //もしタイマーが実行中だったらスタートしない
+            if unwrapedTimerHandler.isValid == true{
+                //何も処理しない
+                return
+            }
+        }
+        //残り時間が0以下の時count（経過時間）を0に初期化する
+        if timerValue - count <= 0 {
+            //countを0に初期化する
+            count = 0
+        }
+        
+        //タイマーをスタート
+        timerHandler = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            //タイマー実行時に呼び出される
+            //1秒毎に実行されてカウントダウンする関数を実行する
+            countDownTimer()
+        }
+    }//startTimer()ここまで
+    }// 　ここまで
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
+
--- My Time/SettingView.swift
+++ My Time/SettingView.swift
@@ -8,10 +8,42 @@
 import SwiftUI
 
 struct SettingView: View {
+    //永続化する秒数設定（初期値は10）
+    @AppStorage("timer_value") var timerValue = 10
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        ZStack{
+            Color("backgroundSetting")
+                .ignoresSafeArea()
+            //垂直にレイアウト
+            VStack{
+                Spacer()
+                Text("\(timerValue)秒")
+                    .font(.largeTitle)
+                    Spacer()
+                //pickerを表示
+                Picker(selection: $timerValue){
+                    Text("10")
+                        .tag(10)
+                    Text("20")
+                        .tag(20)
+                    Text("30")
+                        .tag(30)
+                    Text("40")
+                        .tag(40)
+                    Text("50")
+                        .tag(50)
+                    Text("60")
+                        .tag(60)
+                } label: {
+                    Text("選択")
+                }
+                //pickerをホイール表示
+                .pickerStyle(.wheel)
+            }//VStack
+        }//ZStack
+    }//body
+}//SettingView
 
 struct SettingView_Previews: PreviewProvider {
     static var previews: some View {
